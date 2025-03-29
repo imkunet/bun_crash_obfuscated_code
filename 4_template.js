3e054a1a5cfc3c879cfe6139e4c0bd86a6b92f02
imkunet <im@kunet.dev>
2025-03-29 19:57:33 -0400

    Initial commit
    feat: add files

---
+++ 4_template.js
@@ -0,0 +1,18 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+const person = 'Mike';
+const age = 28;
+
+function myTag(strings, personExp, ageExp) {
+  const str0 = strings[0]; // "That "
+  const str1 = strings[1]; // " is a "
+  const str2 = strings[2]; // "."
+
+  const ageStr = ageExp < 100 ? 'youngster' : 'centenarian';
+
+  // We can even return a string built using a template literal
+  return `${str0}${personExp}${str1}${ageStr}${str2}`;
+}
+
+const output = myTag`That ${person} is a ${age}.`;
+console.log(output);
---
+++ README.md
@@ -0,0 +1,5 @@
+# bun_crash_obfuscated_code
+
+`4_template.obf.js` - if you run it using bun will crash
+
+`4_template.js` is the original file for reference
